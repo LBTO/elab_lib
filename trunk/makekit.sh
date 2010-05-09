c54ad74e9567fef0ddbf5a6a5cf52e8dd8b9330d
Luca Fini <luca.fini@inaf.it>
2010-05-09 00:57:50 +0000

    Small enhancement to Makefile: removing older files when installing conf and calib Small enhancement to makekit.sh
    git-svn-id: svn+ssh://adopt/aogroup/svn/elab_lib/trunk@249 c96302b3-43d6-4c9a-9402-a5495c469ad8

--- trunk/makekit.sh
+++ trunk/makekit.sh
@@ -8,12 +8,13 @@ echo
 echo -n "Continue? [yes/no] "
 read answer && if [ "$answer" = "yes" ] ; then \
    make clean ; \
+   make clean-contrib ; \
    DATE=`date +%Y-%m-%d` ; \
    SOURCETAR=AoSup_$DATE.tgz ; \
    CONFTAR=AoConf_$DATE.tgz ; \
    tar cvfz /tmp/$SOURCETAR --exclude=.svn --exclude CVS --exclude=calib --exclude=conf .  ; \
    mv /tmp/$SOURCETAR .  ; \
-   tar cvfz /tmp/$CONFTAR --exclude=.svn --exclude CVS ./conf ; \
+   tar cvfz /tmp/$CONFTAR --exclude=.svn --exclude CVS ./conf ./calib ; \
    mv /tmp/$CONFTAR .  ; \
    echo; echo "Now you may want to move the kit files: $SOURCETAR, $CONFTAR"; \
    echo "somewhere else: otherwise they will be deleted by next \"make clean\""; echo ;\
