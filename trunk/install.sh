3453f3fc59c99f4b594effad1fedcaf3d5bb1d35
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-01-28 13:52:07 +0000

    Added installation script
    git-svn-id: svn+ssh://adopt/aogroup/svn/elab_lib/trunk@5517 c96302b3-43d6-4c9a-9402-a5495c469ad8

---
+++ trunk/install.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Elab_lib installation script.
+#
+# Usage:
+#
+# ./install.sh: install in default IDL library directory
+#               (you will probably need root privileges)
+#
+# ./install.sh <path>: install in the specified path
+#
+# Written by: A: Puglisi, Jan 2020
+
+case $# in
+   0) 
+      echo
+      echo "Running IDL to detect the library dir..."
+      echo
+      IDLDIR=`idl -e "print, pref_get('IDL_DIR')" 2>/dev/null`
+      INSTALLDIR=${IDLDIR}/lib/elab_lib
+      ;;
+   1) INSTALLDIR=$1 ;;
+   *) echo "Too many arguments specified"
+      exit 1
+      ;;
+esac
+
+echo
+echo "Elab_lib will be installed in this directory:"
+echo
+echo ${INSTALLDIR}
+echo
+
+read -p "Continue? (y/n) " yesno
+first=`echo $yesno | cut -c 1`
+
+if [ $first = "y" ]; then
+
+    echo
+    echo -n "Installing in $INSTALLDIR ... "
+    install -d $INSTALLDIR
+    cp -a *.pro $INSTALLDIR
+
+    echo "done!"
+    echo
+
+fi
